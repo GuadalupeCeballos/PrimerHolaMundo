5462c0ae34a3ce9e5f95b894c3f5de1a9c92dc86
GuadalupeCeballos <150336910+GuadalupeCeballos@users.noreply.github.com>
2023-11-09 22:36:21 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+import requests 
+from streamlit_lottie import st_lottie
+from PIL import Image
+
+with st.container():
+  st.title('Hola Mundo!')
+  st.write(
+    '''Mi nombre es Ma. Guadalupe Ceballos Maldonado
+    soy estudiante en Ing. en sistemas computacionales y 
+    curso mi 9 semestre. 
+    Esta es una pagina web para la materia de Fundamentos 
+    de análisis de datos con python.  
+    ''')
+  
